48320a201c2fbe0aebc268ffc65868dec7ae3897
Gaëtan levoux <gaetan@faure-levoux.fr>
2025-12-12 15:15:12 +0100

    add database connection file

---
+++ database/db.py
@@ -0,0 +1,26 @@
+import psycopg2
+from config.db import settings
+
+def selectFromDb():
+    
+    try:
+        conn = psycopg2.connect(
+            dbname=settings.DB_NAME,
+            user=settings.DB_USER,
+            password=settings.DB_PASSWORD,
+            host=settings.DB_HOST,
+            port=settings.DB_PORT
+        )
+        print("Connexion réussie !")
+
+        cur = conn.cursor()
+        cur.execute("SELECT * from films;")
+        print(cur.fetchone())
+        print(cur.fetchone())
+        print(cur.fetchone())
+
+        cur.close()
+        conn.close()
+
+    except Exception as e:
+        print("Erreur :", e)
